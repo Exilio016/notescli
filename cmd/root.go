61c8db54875d78ff819fd27a95a2b2b4132959b0
brunofl <brunofl@amdocs.com>
2024-07-16 18:03:40 -0300

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,82 @@
+/*
+Copyright (C) 2024 Bruno Flávio Ferreira
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"log"
+	"notescli/cmd/snippet"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "notescli",
+	Short: "A CLI application to manage notes",
+	Long: `notescli is an application created to help you manage and use your notes more efficiently.
+	This tool provides helpful commands to manage your notes and snippets.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+var cfgFile = ""
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+			"config file (default is $HOME/.config/notescli/config.yaml)")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	viper.SetDefault("snippetsdir", home + "/Notes/snippets")
+	viper.SetDefault("editor", "vim")
+
+	rootCmd.AddCommand(snippet.SnippetCmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		viper.AddConfigPath(home + "/.config/notescli")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		log.Println(err)
+	}
+}
+
+
---
+++ cmd/snippet/snippet.go
@@ -0,0 +1,110 @@
+/*
+Copyright (C) 2024 Bruno Flávio Ferreira
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package snippet
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+
+	"github.com/atotto/clipboard"
+	"github.com/koki-develop/go-fzf"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type Snippet struct {
+	name string
+	content string
+}
+
+var snippets = []Snippet{}
+var mutex sync.Mutex
+
+var SnippetCmd = &cobra.Command{
+	Use: "snippet",
+	Short: "Find and copy a snippet of code",
+	Long: "An fzf-like menu to find a snippet of code and copy it to clipboard",
+	Run: func(cmd *cobra.Command, args []string) {
+		dir := getSnippetDir()
+		processFilesInDir(dir)
+		
+		for _,i := range searchKeys() {
+			clipboard.WriteAll(snippets[i].content)
+		}
+	},
+}
+
+func getSnippetDir() *os.File {
+	path := viper.GetString("snippetsdir")
+	dir, err := os.Open(path)
+	if err != nil {
+		fmt.Fscanln(os.Stderr, "Could not find snippet folder")
+		os.Exit(1)
+	}
+	return dir
+}
+
+func searchKeys() []int {
+	f, err := fzf.New(fzf.WithHotReload(&mutex))
+	cobra.CheckErr(err)
+	res, err := f.Find(&snippets, func(i int) string { return snippets[i].name }, 
+		fzf.WithPreviewWindow(func(i, width, height int) string {
+			return snippets[i].content
+	}))
+	cobra.CheckErr(err)
+	return res
+}
+
+func processFilesInDir(dir *os.File) {
+	children, err := dir.Readdir(0)
+	cobra.CheckErr(err)
+	for _,stat := range children {
+		if !stat.IsDir() {
+			if f, err := os.Open(dir.Name() + "/" + stat.Name()); err == nil {
+				if content, err := io.ReadAll(f); err == nil {
+					go parseContent(string(content), stat.Name())
+				}
+			}
+		}
+	}
+}
+
+func parseContent(content string, filename string) {
+	keyRegex := regexp.MustCompile("(?m)\\s*#{3} (.*)$")
+	valueRegex := regexp.MustCompile("(?s)\\x60{3}\\w*(.*?)(?:\\x60\\x60\\x60)")
+
+	keys := keyRegex.FindAllStringSubmatch(content, -1)
+	values := valueRegex.FindAllStringSubmatch(content, -1)
+
+	if len(keys) != len(values) {
+		fmt.Printf("WARNING: snippet file '%s' malformated! Number of headers doesn't match number of snippets\n", filename)
+		fmt.Printf("Headers: %d, snippets: %d\n", len(keys), len(values))
+	}
+	len := min(len(keys), len(values))
+	for i := 0; i < len; i++ {
+		mutex.Lock()
+		key := keys[i][1]
+		value := strings.Trim(values[i][1], "\r\n\t ")
+		snippets = append(snippets, Snippet{key, value})
+		mutex.Unlock()
+	}
+}
+
